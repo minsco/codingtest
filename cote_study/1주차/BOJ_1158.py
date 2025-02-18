7bf4b58ad8c98a41f291aa3aa58f346c8b64fbcb
minsco <msbg6349@gmail.com>
2025-02-18 20:35:51 +0900

    new file:   "cote_study/1\354\243\274\354\260\250/BOJ_10828.py" new file:   "cote_study/1\354\243\274\354\260\250/BOJ_1158.py" new file:   "cote_study/1\354\243\274\354\260\250/BOJ_18258.py" new file:   "cote_study/1\354\243\274\354\260\250/BOJ_2164.py" new file:   "cote_study/1\354\243\274\354\260\250/BOJ_9012.py" modified:   "cote_study/1\354\243\274\354\260\250/BOJ_1158.py" modified:   "cote_study/1\354\243\274\354\260\250/BOJ_2164.py"

---
+++ cote_study/1주차/BOJ_1158.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+n, k = map(int, input().split())
+q = deque(range(1, n + 1))
+answer = []
+
+while q:
+    for _ in range(k-1):
+        q.append(q.popleft())
+    answer.append(q.popleft())
+
+print(str(answer).replace('[', '<').replace(']', '>'))
