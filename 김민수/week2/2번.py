82b54f332c6871e7a1bcbab495a9e917d11e5ec3
minsco <msbg6349@gmail.com>
2025-02-25 00:37:50 +0900

    modified:   "\352\271\200\353\257\274\354\210\230/week2/2\353\262\210.py" 	new file:   "\352\271\200\353\257\274\354\210\230/week2/4\353\262\210.py" 	new file:   "\352\271\200\353\257\274\354\210\230/week2/5\353\262\210.py"

--- 김민수/week2/2번.py
+++ 김민수/week2/2번.py
@@ -1,35 +1,35 @@
-A = int(input())
-T = int(input())
-N = int(input())
+A = int(input()) 
+T = int(input())  
+N = int(input())  
 
-games = [] # 게임 진행 상황 (튜플로 저장)
-bbun = 1 # 뻔 을 외친 횟수
-degi = 1 # 데기 를 외친 횟수
-cnt = 0 # 몇번째 게임인지 
+turns = [] 
+bun_count = 1 
+deg_count = 1 
+round_number = 1 
 
-while(True):
-    num = bbun # 이전 회차에서 뻔 or 데기를 외친 누적 횟수
-    cnt += 1
-    
-    # 1) 처음에 뻔 - 데기 - 뻔 - 데기 이 4번 반복은 동일함
+while True:
+
     for _ in range(2):
-        games.append((bbun, 0))
-        bbun += 1
-        games.append((degi, 1))
-        degi +=1 
+        turns.append((bun_count, 0))  # "뻔"
+        bun_count += 1
+        turns.append((deg_count, 1))  # "데기"
+        deg_count += 1
 
-    # 2) 뻔 - 뻔 - 반복부분
-    # (1): 2번, (2): 3번, (cnt): cnt+1번
-    for _ in range(cnt+1):
-        games.append((bbun, 0))
-        bbun +=1 
-    
-    for _ in range(cnt+1):
-        games.append((degi, 1))
-        degi += 1
-        
-    # 3) 정답 찾기
-    # 4 <= 6 < 9: 2회차에서 찾아야 함 
-    if (num <= T < bbun):
-        print(games.index((T, N)) % A)
-        break
+
+    for _ in range(round_number + 1):
+        turns.append((bun_count, 0))  # "뻔"
+        bun_count += 1
+
+
+    for _ in range(round_number + 1):
+        turns.append((deg_count, 1))  # "데기"
+        deg_count += 1
+
+ 
+    if turns[-1][0] >= T:  
+        for i in range(len(turns)): 
+            if turns[i] == (T, N):  
+                print(i % A)  
+                exit()
+
+    round_number += 1  
